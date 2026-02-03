7f8a051bce1e98a7185f3964f0df62d1c56e1cd3
Evan Kristian Pratama <if2372047@student.it.maranatha.edu>
2026-02-03 22:55:13 +0700

    Fix: vite.config.js syntax error

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 import tailwindcss from '@tailwindcss/vite';
+import fs from 'fs';
 
 export default defineConfig({
     plugins: [
@@ -23,6 +24,6 @@ export default defineConfig({
         tailwindcss(),
     ],
     define: {
-        __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
+        __APP_VERSION__: JSON.stringify(JSON.parse(fs.readFileSync('package.json', 'utf-8')).version),
     },
 });
